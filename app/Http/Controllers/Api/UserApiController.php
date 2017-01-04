aa382ce364b6946c4b1833b85bd22daa128c460a
Devi Shankar <devishankar@Devi-Shankar487.local>
2017-01-04 14:10:10 +0530

    firt commit

---
+++ app/Http/Controllers/Api/UserApiController.php
@@ -0,0 +1,190 @@
+<?php
+namespace App\Http\Controllers\api;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+use DB;
+use Hash;
+use App\Models\User;
+use App\Models\UserGroup;
+use App\Models\Device;
+use App\Models\UserProfile;
+use Mail;
+use Auth;
+class UserApiController extends Controller {
+    
+    public function __construct() {
+        
+    }
+    
+    public function postSignup(Request $request){
+        try {
+            $this->validate($request, [
+                'deviceId' => 'required',
+                'deviceType' => 'required',
+                'deviceToken' => 'required',
+                'firstName' => 'required',
+                'lastName' => 'required',
+                'email' => 'required',
+                'password' => 'required',
+                'prefferedLocation' => 'required',
+                'latitude' => 'required',
+                'longitude' => 'required',
+                'zipCode' => 'required',
+            ]);
+        } catch (ValidationException $e) {
+            $messages = json_decode($e->getResponse()->content(), true);
+            return $this->responseError("Request validation failed.", ["data" => $messages]);
+        }
+        $reqData = $request->all();
+        $userExists = User::where('email', $reqData['email'])->first();
+        if($userExists){
+            $response = $this->customJsonResponse(0, 201, "User already exists with this email");      
+        }else{
+            $user =  array(
+                'email' => $reqData['email'],
+                'password' => bcrypt($reqData['password']),
+            );
+            $user_details = User::create($user);
+            
+            $userGroupModel = new UserGroup();
+            $userGroupModel->group_id = 3;
+            $userGroupModel->user_id = $user_details->id;
+            $userGroupModel->save();
+            
+            $userProfileModel = new UserProfile();
+            $userProfileModel->user_id = $user_details->id;
+            $userProfileModel->first_name = $reqData['firstName'];
+            $userProfileModel->last_name = $reqData['lastName'];
+            $userProfileModel->save();
+            
+            $deviceModel =  new Device();
+            $reqData['deviceOs'] = isset($reqData['deviceOs'])?$reqData['deviceOs']:'';
+            $reqData['appVersion'] = isset($reqData['appVersion'])?$reqData['appVersion']:'';
+            $user_token=$deviceModel->register_device(
+                    $reqData['deviceId'],
+                    $user_details->id,
+                    $reqData['deviceToken'],
+                    $reqData['deviceType'],
+                    $reqData['deviceOs'],
+                    $reqData['appVersion']);
+            
+            /*Mail::queue('email.resetPasswordToken', ['name' => $reqData['firstName'], 'url' => url('user-activation', ['token' => md5($reqData['email'] . time())]), 'email' => $reqData['email']], function($message)  {
+                    $message->to($user->email, $user->first_name)->subject('Reset Password Request ');
+                });*/
+            
+            $response = $this->customJsonResponse(1, 200, "User registered successfully"); 
+        }
+        return $response;
+    }
+    
+    /**
+     *  Method to find log-in the user via POST method
+     * @param Request $request
+     * @return type
+     */
+    public function postSignIn(Request $request) {
+        
+        try {
+            $this->validate($request, [
+                'deviceId' => 'required',
+                'deviceType' => 'required',
+                'email' => 'required|email',
+                'password' => 'required', 
+                'userType' => 'required',
+                'deviceToken' => 'required',
+            ]);
+        } catch (ValidationException $e) {
+            $messages = json_decode($e->getResponse()->content(), true);
+            return $this->responseError("Request validation failed.", ["data" => $messages]);
+        }
+        $reqData = $request->all();
+        $userAttempt = Auth::attempt(['email' => $reqData['email'], 'password' => $reqData['password']]);
+        $user_id = ($userAttempt==true) ? Auth::user()->id : null;
+        if($user_id > 0){
+            $user_data = User::join('user_groups', 'user_groups.user_id', '=', 'users.id')
+                        ->join('jobseeker_profiles','jobseeker_profiles.user_id' , '=','users.id')
+                        ->select('user_groups.*', 'users.*','jobseeker_profiles.*')
+                        ->where('users.id', $user_id)
+                        ->first();
+            
+            if($user_data['group_id'] == 3){
+                $device = Device::where('user_id', $user_id)->orWhere('device_id', $reqData['deviceId'])->first();
+                $reqData['deviceOs'] = isset($reqData['deviceOs'])?$reqData['deviceOs']:'';
+                $reqData['appVersion'] = isset($reqData['appVersion'])?$reqData['appVersion']:'';
+                 if (is_object($device) && ($device->device_id != $reqData['deviceId'] || $device->user_id != $user_id)) {
+                    Device::where('device_id', $device->device_id)->orWhere('user_id', $user_id)->delete();
+                    $deviceModel = new Device();
+                    $user_token = $deviceModel->register_device($reqData['deviceId'], $user_id, $reqData['deviceToken'], $reqData['deviceType'], $reqData['deviceOs'], $reqData['appVersion']);
+                    $user_data->userToken = $user_token;
+                } else {
+                    $deviceModel = new Device();
+                    $user_token = $deviceModel->register_device($reqData['deviceId'], $user_id, $reqData['deviceToken'], $reqData['deviceType'], $reqData['deviceOs'], $reqData['appVersion']);
+                    $user_data->userToken = $user_token;
+                }
+                $user_array['userDetails'] = (array)$user_data;
+                $response = $this->customJsonResponse(1, 200, "User loggedin successfully",$user_array); 
+            }else{
+                $response = $this->customJsonResponse(0, 201, "Invalid login credentials"); 
+            }
+        }else{
+            $response = $this->customJsonResponse(0, 201, "Invalid login credentials"); 
+        }
+        return $response;
+    }
+    
+    public function getTermsAndCondition() {
+        return view('terms-and-condition');   
+    }
+    
+    public function getPrivacypolicy() {
+        return view('privacy-policy');   
+    }
+
+
+    public function responseError($message = '', $data = array()) {
+        $key = !empty($data) ? key($data) : '';
+        $response = array(
+            'status' => 0,
+            'message' => $message
+        );
+        if (!empty($key)) {
+            $response[$key] = (object) $data[$key];
+        }
+        return $this->convertToCamelCase($response);
+    }
+    
+    protected function convertToCamelCase($array) {
+        $converted_array = [];
+        foreach ($array as $old_key => $value) {
+            if (is_array($value)) {
+                $value = $this->convertToCamelCase($value);
+            } else if (is_object($value)) {
+                if (method_exists($value, 'toArray')) {
+                    $value = $value->toArray();
+                } else {
+                    $value = (array) $value;
+                }
+
+
+                $value = $this->convertToCamelCase($value);
+            }
+            $converted_array[camel_case($old_key)] = $value;
+        }
+
+        return $converted_array;
+    }
+    
+    public function customJsonResponse($status, $statusCode, $message = '', $data = array()) {
+        $response = array(
+            'status' => $status,
+            'statusCode' => $statusCode,
+            'message' => $message,
+        );
+        if(is_array($data) && count($data) > 0){
+            $response['result'] = (object) $data;
+        }
+        return json_encode($response);
+    }
+    
+}
---
+++ app/Models/device.php
@@ -0,0 +1,75 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Device extends Model  {
+    protected $table        = 'devices';
+    protected $primaryKey   = 'id';
+    
+    protected $fillable     = ['id','device_id','user_id','device_token','device_type','device_os','application_version','user_token'];
+    
+
+    public static function unRegister_single($request){
+       Device::where('user_id',$request->input('user_id'))->where('device_id',$request->input('deviceId'))->delete();
+       return true;
+    }
+    public static function unRegister_all($user_id){
+       Device::where('user_id',$user_id)->delete();
+       return true;
+    }
+    public function register_device($device_id, $user_id, $device_token, $device_type,$deviceOs='',$appVersion='') {
+        $user_token = md5($device_id);
+        $data = array(
+            'device_id' => $device_id,
+            'user_id' => $user_id,
+            'device_token' => $device_token,
+            'device_type' => $device_type,
+            'user_token'=>$user_token,
+            'device_os'=>$deviceOs,
+            'application_version'=>$appVersion
+                );
+        $device = Device::firstOrNew(array('device_id' => $device_id, 'user_id' => $user_id));
+        $device->fill($data);
+        $device->save();
+        return $user_token;
+    }
+    
+    public static function getDeviceToken($user_id) {
+        
+        return Device::where('user_id',$user_id)
+                ->join('users','users.id','=','devices.user_id')
+                ->where('users.is_active',1)
+                ->whereRaw('device_token IS NOT NULL AND device_token!=""')
+                ->orderBy('dev_id','desc')->first();
+    }
+    
+    public static function getAllDeviceToken($userGroup=1,$messageType='2'){
+        $deviceObj = static::whereRaw('device_token IS NOT NULL AND device_token!=""');
+        if($messageType=='1'){
+            if($userGroup=='1'){
+                $deviceObj->join('events',function($join){
+                        $join->on('events.consumer_id','=','devices.user_id');
+                        $join->orOn('events.photographer_id','=','devices.user_id');
+                    });
+            }else{
+                $column = ($userGroup=='2')?'photographer_id':'consumer_id';
+                $deviceObj->join('events','events.'.$column,'=','devices.user_id');
+            }
+            $deviceObj->whereRaw('users.created_at <= DATE_SUB(CURDATE(),INTERVAL 30 DAY)');
+        }else{
+            $deviceObj->join('user_groups','user_groups.user_id','=','devices.user_id');
+            if($userGroup=='1'){
+                $deviceObj->whereIn('group_id',['2','3']);
+            }else{
+                $deviceObj->where('group_id',$userGroup);
+            }
+        }
+        return $deviceObj->join('users','users.id','=','devices.user_id')
+                ->where('users.is_active',1)
+                ->select('devices.user_id','device_token')->distinct('device_token')->get();
+    }
+    // clear badge method
+    
+}
+
