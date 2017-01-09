0abcf8be3cc2d6a6a7bc8cd7caefe72e992c9b8f
Devi Shankar <devishankar@Devi-Shankar487.local>
2017-01-09 11:08:43 +0530

    change route for job listing api

--- routes/api.php
+++ routes/api.php
@@ -23,4 +23,4 @@ Route::get('privacy-policy','Api\UserApiController@getPrivacypolicy');
 Route::get('term-condition','Api\UserApiController@getTermsAndCondition');
 Route::put('users/forgot-password', 'api\UserApiController@putForgotPassword');
 Route::put('users/reset-password', 'api\UserApiController@putForgotPassword');
-Route::get('skillList','Api\MasterApiController@getSkilllists');
+Route::get('list-jobtitle','Api\MasterApiController@getSkilllists');
