cd61bb9d17b693c072e240c8e4b12292a6b811db
Ashish Ginotra <ashish.ginotra@appster.in>
2017-01-09 14:50:46 +0530

    upload image api

--- app/Http/Controllers/Api/UserProfileApiController.php
+++ app/Http/Controllers/Api/UserProfileApiController.php
@@ -31,10 +31,16 @@ class UserProfileApiController extends Controller {
         try {
             $userId = apiResponse::loginUserId($request->header('accessToken'));
             $filename = $this->generateFilename($userId, $request->type);
-            echo $filename;
-            $res = $this->uploadFileToAWS($request, $filename);
-            if ($res) {
-                echo 'photo upload';
+            $response = $this->uploadFileToAWS($request, $filename);
+            if ($response['res']) {
+                $file = str_replace($request->type . '/', '', $response['file']);
+                if ($request->type == 'profile_pic') {
+                    UserProfile::where('user_id', $userId)->update(['profile_pic' => $file]);
+                } else {
+                    UserProfile::where('user_id', $userId)->update(['dental_state_board' => $file]);
+                }
+                $url['img_url'] = env('AWS_URL') . '/' . env('AWS_BUCKET') . '/' . $response['file'];
+                return apiResponse::customJsonResponse(1, 200, "Image Saved successfully", $url);
             } else {
                 return apiResponse::responseError("Problem in uploading image.");
             }
