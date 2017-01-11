823e298f35d650bcb38f22d113b84724e1cdb820
ravi_madan <ravi.madan@appster.in>
2017-01-11 12:33:18 +0530

    work experience

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -1,7 +1,8 @@
 <?php
 
 return [
-    'credentials_not_matched'=>'These credentials do not match our records.'
-   
-
+    'credentials_not_matched'=>'These credentials do not match our records.',
+    'work_exp_list' =>"Work Experience List Retrieved",
+    'something_wrong' => 'Something went wrong',
+    
 ];
