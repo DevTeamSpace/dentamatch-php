cb7a279dfbc4997acde119703a09f9d37cbd091f
ravi_madan <ravi.madan@appster.in>
2017-01-11 12:42:46 +0530

    unused namespaces removed

--- app/Http/Controllers/Api/UserApiController.php
+++ app/Http/Controllers/Api/UserApiController.php
@@ -3,14 +3,11 @@ namespace App\Http\Controllers\api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Validation\ValidationException;
-use DB;
-use Hash;
 use App\Models\User;
 use App\Models\UserGroup;
 use App\Models\Device;
 use App\Models\UserProfile;
 use App\Models\PasswordReset;
-use App\Models\JobTitles;
 use Mail;
 use Auth;
 use App\Helpers\apiResponse;
@@ -241,7 +238,4 @@ class UserApiController extends Controller {
         }
         
     }
-    
-    
-    
 }
