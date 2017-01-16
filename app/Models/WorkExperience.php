5e1751bdf27785c1587d047a7969be8294381ee0
ravi_madan <ravi.madan@appster.in>
2017-01-16 08:45:53 +0530

    word exp delete soft delete

--- app/Models/WorkExperience.php
+++ app/Models/WorkExperience.php
@@ -3,8 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class WorkExperience extends Model {
+    use SoftDeletes;
 
     protected $table = 'jobseeker_work_experiences';
     protected $primaryKey = 'id';
