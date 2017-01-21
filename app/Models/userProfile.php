c7d7a3ebb9429cf7cf87aee6f85af516d2b7f088
ravi_madan <ravi.madan@appster.in>
2017-01-21 11:36:00 +0530

    user profile adress data

--- app/Models/userProfile.php
+++ app/Models/userProfile.php
@@ -21,8 +21,8 @@ class UserProfile extends Model {
         $s3Url = env('AWS_URL');
         $s3Bucket = env('AWS_BUCKET');
         
-        $userModel = static::select('id', 'user_id', 'first_name', 'last_name', 
-                        'profile_pic', 'dental_state_board', 'license_number', 'state', 'about_me')
+        $userModel = static::select('id', 'user_id', 'first_name', 'last_name', 'zipcode', 'latitude', 'longitude', 'preferred_job_location', 
+                            'job_titile_id', 'profile_pic', 'dental_state_board', 'license_number', 'state', 'about_me')
                     ->where('user_id', $userId)
                     ->first();
         
