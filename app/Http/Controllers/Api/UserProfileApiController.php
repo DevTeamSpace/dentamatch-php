f3bca55c9f07e5c60dab1ba5b83ffec8bc4fab37
ravi_madan <ravi.madan@appster.in>
2017-01-25 15:35:43 +0530

    joblist in user profile

--- app/Http/Controllers/Api/UserProfileApiController.php
+++ app/Http/Controllers/Api/UserProfileApiController.php
@@ -14,6 +14,7 @@ use App\Models\JobSeekerSkills;
 use App\Models\JobSeekerAffiliation;
 use App\Models\JobseekerCertificates;
 use App\Models\Certifications;
+use App\Models\JobTitles;
 
 class UserProfileApiController extends Controller {
 
@@ -211,7 +212,7 @@ class UserProfileApiController extends Controller {
                                                                     ];
                     }
                 }
-                
+                $jobTitle = JobTitles::where('is_active',1)->get()->toArray();
                 $affiliations = JobSeekerAffiliation::getJobSeekerAffiliation($userId);
                 $jobSeekerCertifications = JobseekerCertificates::getJobSeekerCertificates($userId);
                 $allCertification = Certifications::getAllCertificates();
@@ -234,6 +235,7 @@ class UserProfileApiController extends Controller {
                 $data['affiliations'] = $affiliations;
                 $data['certifications'] = array_values($certificationData);
                 $data['workExperience'] = $userWorkExperience;
+                $data['joblists'] = $jobTitle;
                 
                 $response =  apiResponse::customJsonResponse(1, 200, trans("messages.user_profile_list"), apiResponse::convertToCamelCase($data));
             }else{
