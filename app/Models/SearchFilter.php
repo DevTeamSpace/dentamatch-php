b1a456786743efec82ea043805f4aa672f85bdfb
ravi_madan <ravi.madan@appster.in>
2017-02-02 13:14:41 +0530

    search filter post data during search jobs

---
+++ app/Models/SearchFilter.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class SearchFilter extends Model
+{
+    use SoftDeletes;
+  
+    protected $table  = 'search_filters';
+    protected $primaryKey = 'id';
+    protected $dates = ['deleted_at'];
+    /**
+    * The attributes that should be hidden for arrays.
+    *
+    * @var array
+    */
+    protected $hidden = [
+       'updated_at', 'deleted_at'
+    ];
+    
+    public static function createFilter($userId, $data)
+    {
+        $searchFilterModel = static::where('user_id', $userId)->first();
+        if(!$searchFilterModel)
+        {
+            $searchFilterModel = new SearchFilter();
+        }
+        $searchFilterModel->search_filter = json_encode($data);
+        $searchFilterModel->user_id = $userId;
+        $searchFilterModel->save();
+    }
+    
+
+}
