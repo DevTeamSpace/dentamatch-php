38caa5f8f3eceb61ed8bba5f530023ac8c6cf5be
Devi Shankar <devishankar@Devi-Shankar487.local>
2017-02-02 12:33:43 +0530

    update search api

--- app/Models/RecruiterJobs.php
+++ app/Models/RecruiterJobs.php
@@ -63,10 +63,12 @@ class RecruiterJobs extends Model
                 }
         $latitude = $reqData['lat'];
         $longitude = $reqData['lng'];
-                $searchQueryObj = RecruiterJobs::join('recruiter_offices', 'recruiter_jobs.recruiter_office_id', '=', 'recruiter_offices.id')
+                $searchQueryObj = RecruiterJobs::leftJoin('job_lists','job_lists.recruiter_job_id','=','recruiter_jobs.id')
+                        ->join('recruiter_offices', 'recruiter_jobs.recruiter_office_id', '=', 'recruiter_offices.id')
                         ->join('job_templates','job_templates.id','=','recruiter_jobs.job_template_id')
                         ->join('job_titles','job_titles.id', '=' , 'job_templates.job_title_id')
                         ->join('recruiter_profiles','recruiter_profiles.user_id', '=' , 'recruiter_offices.user_id')
+                        ->whereIsNull('job_lists.id')
                         ->whereIn('job_titles.id', $reqData['jobTitle']);
                 if($reqData['isFulltime'] == 1 && $reqData['isParttime'] == 0){
                     $searchQueryObj->where('recruiter_jobs.job_type',1);
