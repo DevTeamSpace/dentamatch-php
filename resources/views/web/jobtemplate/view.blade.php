fc424b34b8954a05eb7fb3e02772e0b623f8bb2e
Chander bhatt <chander.bhatt@appster.in>
2017-02-03 16:32:53 +0530

    view page is fixed with layout

--- resources/views/web/jobtemplate/view.blade.php
+++ resources/views/web/jobtemplate/view.blade.php
@@ -5,35 +5,35 @@
 @endsection
 @section('content')
     <div class="container padding-container-template">
-            <ul class="breadcrumb breadcrumb-custom">
+            <ul class="breadcrumb">
                 <li>Template</li>
                 <li class="active">{{ $templateData->templateName }}</li>
             </ul>
-            <div class="template">
-                <div class="template-header">
-                    <h1>{{ $templateData->templateName }}</h1>
+            <div class="row sec-mob mr-b-10">
+                <div class="col-sm-6 col-xs-6">
+                    <div class="section-title mr-b-10">{{ $templateData->templateName }}</div>
                 </div>
-                <div class="template-header-right">
-                    <a href="{{ url('createJob/'.$templateId)}}" class="btn btn-primary">Create Job Opening</a>
+                <div class="col-sm-6 text-right col-xs-6">
+                    <a href="{{ url('createJob/'.$templateId)}}" class="btn btn-primary pd-l-25 pd-r-25">Create Job Opening</a>
                 </div>  
             </div>    
-            <div class="viewdentaltemplate">
-                <div class="profile-div">
+            <div class="commonBox">
+                <div class="viewtemp-div mr-b-30">
                     <div class="dropdown icon-upload-ctn1">
                         <span class="icon-upload-detail dropdown-toggle" data-toggle="dropdown" aria-expanded="true"></span>
                         <ul class="actions text-left dropdown-menu">
-                            <li class="text-center">
+                            <li>
                                 <a href="{{ url('jobtemplates/edit/'.$templateId) }}">
-                                    <span class="delete-icon">
-                                        <img src="{{asset('web/images/denatmatch-shape.png')}}" alt="delta delete icon" width="12">
+                                    <span class="gbllist iconFirstEdit">
+                                       <i class="icon icon-edit"></i>  Edit
                                     </span>
-                                    <span class="cancel">Edit</span>
+                                 
                                 </a>
                             </li>
                         </ul>
                     </div>
                     <div class="searchResultHeading">
-			<h5>Job Title</h5>
+			<h6><strong>Job Title</strong></h6>
 			<p>{{ $templateData->jobtitle_name }}</p>
 		</div>
 <!--                    <div class="title">
@@ -47,18 +47,18 @@
                         <p class="title-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quorum sine causa fieri nihil putandum est. Potius inflammat, ut coercendi magis quam dedocendi esse videantur. Duo Reges: constructio interrete. Mihi enim satis est, ipsis non satis. Cuius ad naturam apta ratio vera illa et summa lex a philosophis dicitur. Id est enim, de quo quaerimus. Id quaeris, inquam, in quo, utrum respondero, verses te huc atque illuc necesse est.</p>
                         <p class="title-description">Sed ad bona praeterita redeamus. Roges enim Aristonem, bonane ei videantur haec: vacuitas doloris, divitiae, valitudo; Nam prius a se poterit quisque discedere quam appetitum earum rerum, quae sibi conducant, amittere.</p>
                     </div>-->
-                    <div class="searchResultHeading">
-			<h5>Job Description</h5>
+                    <div class="viewtemp-div mr-b-30">
+			<h6><strong>Job Description</strong></h6>
 			<p>{{ $templateData->templateDesc }}</p>
 		</div>
                 </div> 
-                <div class="profile-div">
-                   <div class="searchResultHeading">
-			<h5>KEY SKILLS</h5>
+                <div class="viewtemp-div">
+                   <div class="viewtemp-div">
+			<h6 class="mr-b-30"><strong>KEY SKILLS</strong></h6>
 			</div>
 			@foreach($templateSkillsData as $index=>$skillsData)
-                            <div class="{{ ($index=='0'?'':'pd-t-10') }}  keySkills">
-                                <b>{{ $skillsData['parent_skill_name']}}</b>
+                            <div class="{{ ($index=='0'?'':'pd-t-5') }}  keySkills">
+                                <h6><strong>{{ $skillsData['parent_skill_name']}}</strong></h6>
                                 <p>{{ $skillsData['skill_name']}}</p>
                            </div>
                         @endforeach
