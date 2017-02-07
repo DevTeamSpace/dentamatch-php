7f57521bb8627998ce8fcf38c192cec99f85445c
jasvinderSingh <jasvinder.singh@appster.in>
2017-02-07 15:30:38 +0530

    detail page issue

--- resources/views/web/recuriterJob/view.blade.php
+++ resources/views/web/recuriterJob/view.blade.php
@@ -178,13 +178,13 @@
                             <!--<span class="bg-ember statusBtn mr-r-5">Part time</span>-->
                             @php 
                             $seekerDayArr = [];
-                            ($seeker['is_monday']==1)?array_push($seekerDayArr,'Monday'):'';
-                            ($seeker['is_tuesday']==1)?array_push($seekerDayArr,'Tuseday'):'';
-                            ($seeker['is_wednesday']==1)?array_push($seekerDayArr,'Wednesday'):'';
-                            ($seeker['is_thursday']==1)?array_push($seekerDayArr,'Thursday'):'';
-                            ($seeker['is_friday']==1)?array_push($seekerDayArr,'Friday'):'';
-                            ($seeker['is_saturday']==1)?array_push($seekerDayArr,'Saturday'):'';
-                            ($seeker['is_sunday']==1)?array_push($seekerDayArr,Sunday):'';
+                            ($seeker['is_parttime_monday']==1)?array_push($seekerDayArr,'Monday'):'';
+                            ($seeker['is_parttime_tuesday']==1)?array_push($seekerDayArr,'Tuseday'):'';
+                            ($seeker['is_parttime_wednesday']==1)?array_push($seekerDayArr,'Wednesday'):'';
+                            ($seeker['is_parttime_thursday']==1)?array_push($seekerDayArr,'Thursday'):'';
+                            ($seeker['is_parttime_friday']==1)?array_push($seekerDayArr,'Friday'):'';
+                            ($seeker['is_parttime_saturday']==1)?array_push($seekerDayArr,'Saturday'):'';
+                            ($seeker['is_parttime_sunday']==1)?array_push($seekerDayArr,Sunday):'';
                             @endphp
                             {{ implode(', ',$seekerDayArr) }}
                         </p>
