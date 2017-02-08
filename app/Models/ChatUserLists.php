46112739a21e1d0ae37572cc252fb51d6bb189b6
jasvinderSingh <jasvinder.singh@appster.in>
2017-02-08 08:57:35 +0530

    api chat list user add attribute

--- app/Models/ChatUserLists.php
+++ app/Models/ChatUserLists.php
@@ -31,7 +31,7 @@ class ChatUserLists extends Model
             ->where('chat_user_list.seeker_id',$userId)
             ->groupBy('chat_user_list.seeker_id')
             ->select('recruiter_profiles.office_name as name','chat_user_list.recruiter_id as recruiterId',
-                    'chat_user_list.seeker_id as seekerId','user_chat.id as messageId',
+                    'chat_user_list.id as messageListId','chat_user_list.seeker_id as seekerId','user_chat.id as messageId',
                     'chat_user_list.recruiter_block as recruiterBlock','chat_user_list.seeker_block as seekerBlock',
                     'user_chat.message','user_chat.updated_at as timestamp')->get()->toArray();
     }
