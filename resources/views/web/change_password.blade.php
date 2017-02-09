15a697028075e7018516d159dc5570ecd785c463
jasvinderSingh <jasvinder.singh@appster.in>
2017-02-09 11:44:29 +0530

    change password ui issue

--- resources/views/web/change_password.blade.php
+++ resources/views/web/change_password.blade.php
@@ -7,15 +7,15 @@
         <div class="col-sm-4 col-md-3">
             <div id="ChildVerticalTab_1"  class="cboxbottom">
                 <ul class="resp-tabs-list ver_1 small-border-radius  decoration">
-                    <li class="resp-tab-active"><a href ="#"><span class="icon icon-account-circle  "></span>Profile</a></li>
-                    <li><a href ="#"><span class="icon icon-drive-document "></span>Reports</a></li>
-                    <li><a href ="#"><span class="icon icon-lock "></span>Change Password</a></li>
-                    <li><a href ="#"><span class="icon icon-drive-form "></span>Terms & Conditions</a></li>
+                    <li><a href ="javascript:void(0);"><span class="icon icon-account-circle  "></span>Profile</a></li>
+                    <li><a href ="javascript:void(0);"><span class="icon icon-drive-document "></span>Reports</a></li>
+                    <li class="resp-tab-active"><a href ="javascript:void(0);"><span class="icon icon-lock "></span>Change Password</a></li>
+                    <li><a href ="javascript:void(0);"><span class="icon icon-drive-form "></span>Terms & Conditions</a></li>
                     <li>
-                        <a href ="#">
+                        <a href ="javascript:void(0);">
                             <span class="icon icon-text-document-black-interface-symbol "></span>Subscription Details</a>
                     </li>
-                    <li><a href ="#"><span class="icon icon-logout-web-button "></span>Logout</a></li>
+                    <li><a href ="{{ url('logout') }}"><span class="icon icon-logout-web-button "></span>Logout</a></li>
                 </ul>
             </div>
         </div>
