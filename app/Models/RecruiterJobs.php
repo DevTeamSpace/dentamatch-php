02044c224aa572a1f2c7ef2772fa5b74f5d510e4
Devi Shankar <devishankar@Devi-Shankar487.local>
2017-02-09 10:35:54 +0530

    update search api

--- app/Models/RecruiterJobs.php
+++ app/Models/RecruiterJobs.php
@@ -61,12 +61,12 @@ class RecruiterJobs extends Model
                         return  $value['recruiter_job_id'];
                     }, $savedJobsArray);
                 }
-        //$latitude = $reqData['lat'];
-        //$longitude = $reqData['lng'];
+        $latitude = $reqData['lat'];
+        $longitude = $reqData['lng'];
         
-        $userProfile = UserProfile::where('user_id', $reqData['userId'])->first();
+        /*$userProfile = UserProfile::where('user_id', $reqData['userId'])->first();
         $longitude = $userProfile->longitude;
-        $latitude = $userProfile->latitude;
+        $latitude = $userProfile->latitude;*/
                 /*$searchQueryObj = RecruiterJobs::leftJoin('job_lists','job_lists.recruiter_job_id','=','recruiter_jobs.id')
                         ->join('recruiter_offices', 'recruiter_jobs.recruiter_office_id', '=', 'recruiter_offices.id')
                         ->join('job_templates','job_templates.id','=','recruiter_jobs.job_template_id')
