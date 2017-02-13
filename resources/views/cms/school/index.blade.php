ba4baae60e6d89f6372f101678315b8c3190af3c
Devi Shankar <devishankar@Devi-Shankar487.local>
2017-02-13 12:38:05 +0530

    change in school listing view

--- resources/views/cms/school/index.blade.php
+++ resources/views/cms/school/index.blade.php
@@ -8,7 +8,7 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Skill List
+                <div class="panel-heading">School List
                     <div class="span6 pull-right" style="text-align:right">
                         <a href="{{ URL::to("cms/school/create") }}">Add School</a>
                     </div>
