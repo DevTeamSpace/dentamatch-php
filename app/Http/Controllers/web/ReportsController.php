91b93508e712370ecea2f1a5df27cd70e6062c8c
Rahul <rahul@Rahul485.local>
2017-02-28 07:32:35 +0530

    Changes

--- app/Http/Controllers/web/ReportsController.php
+++ app/Http/Controllers/web/ReportsController.php
@@ -25,21 +25,6 @@ class ReportsController extends Controller
     public function getReportsTempJobs(){
         try{
             $allTempJobs = RecruiterJobs::getTempJobsReports();
-            foreach($allTempJobs as $job){
-                $getJob = RecruiterJobs::getIndividualTempJob($job['job_title_id']);
-                $job['jobs'] = $getJob;
-                foreach($job['jobs'] as $forSeeker){
-                    $jobDetails['id'] = $forSeeker['recruiter_job_id'];
-                    $jobDetails['job_type'] = $forSeeker['job_type'];
-                    $seekers = JobLists::getJobSeekerList($jobDetails, config('constants.OneValue'));
-                    foreach($seekers as $seeker){
-                        foreach($seeker as &$seek){
-                            $seek['profile_pic'] = url("image/" . config('constants.Resolution') . "/" . config('constants.Resolution') . "/?src=" .$seek['profile_pic']);
-                        }
-                    }
-                    $forSeeker['seekers'] = $seekers;
-                }
-            }
             $this->response['data'] = $allTempJobs;
             $this->response['success'] = true;
             $this->response['message'] = trans('messages.reports_temp_jobs');
