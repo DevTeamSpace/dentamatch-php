a60873549e3bfeeb3b341c8f9c56aaffd29e091d
Devi Shankar <devishankar@Devi-Shankar487.local>
2017-03-08 11:22:19 +0530

    update device model

--- app/Models/device.php
+++ app/Models/device.php
@@ -22,7 +22,7 @@ class Device extends Model  {
        return true;
     }
     public function registerDevice($deviceId, $userId, $deviceToken, $deviceType,$deviceOs='',$appVersion='') {
-        $userToken = md5($device_id.$user_id);
+        $userToken = md5($deviceId.$userId);
         $data = array(
             'device_id' => $deviceId,
             'user_id' => $userId,
