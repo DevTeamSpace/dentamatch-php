09e0d8e48eecabd3750851823a8972c31e1282cf
Rahul <rahul@Rahul485.local>
2017-03-16 17:29:52 +0530

    Calender image issue

--- app/Http/Controllers/web/CalenderController.php
+++ app/Http/Controllers/web/CalenderController.php
@@ -23,6 +23,17 @@ class CalenderController extends Controller
     public function getCalenderDetails(){
         try{
             $allJobs = RecruiterJobs::getAllTempJobs();
+            foreach($allJobs as $job){
+                $jobDetails['id'] = $job['id'];
+                $jobDetails['job_type'] = $job['job_type'];
+                $seekers = JobLists::getJobSeekerList($jobDetails, 1);
+                foreach($seekers as &$seeker){
+                    foreach($seeker as &$seek){
+                        $seek['profile_pic'] = url("image/" . 60 . "/" . 60 . "/?src=" .$seek['profile_pic']);
+                    }
+                }
+                $job['seekers'] = $seekers;
+            }
             $this->response['jobs'] = $allJobs;
             $this->response['success'] = true;
             $this->response['message'] = trans('messages.calender_details_fetched');
