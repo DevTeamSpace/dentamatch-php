596d65f624f4908ba1f20db73141384c7e7435c6
jasvinderSingh <jasvinder.singh@appster.in>
2017-03-21 10:56:05 +0530

    chat message issue

--- resources/views/web/recuriterJob/view.blade.php
+++ resources/views/web/recuriterJob/view.blade.php
@@ -266,7 +266,7 @@
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal">&times;</button>
-                <h4 class="modal-title">Invite Jobseeker</h4>
+                <h4 class="modal-title">Messgae</h4>
             </div>
             <div class="modal-body ">
                 <form>
