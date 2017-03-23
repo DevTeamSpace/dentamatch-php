57d123de9e9c8a97d307d36332b1fd9833bfa9f5
Devi Shankar <devishankar@Devi-Shankar487.local>
2017-03-23 10:24:52 +0530

    update for messages

--- app/Http/Controllers/Api/SearchApiController.php
+++ app/Http/Controllers/Api/SearchApiController.php
@@ -303,9 +303,9 @@ class SearchApiController extends Controller {
                         $response = apiResponse::customJsonResponse(1, 200, $msg);
                     }else{
                         if($jobExists->applied_status == JobLists::HIRED){
-                            $msg = "You have already hired for this job";
+                            $msg = "This seeker is already hired for this job";
                         }else{
-                            $msg = "You have already cancelled this job invitation";
+                            $msg = "This seeker is already cancelled for this job";
                         }
                         $response = apiResponse::customJsonResponse(0, 201, $msg);
                     }
