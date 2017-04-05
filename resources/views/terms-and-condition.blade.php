b85cd15d92065d147e5836ba87818281f626f3b2
Ashish Ghildiyal <ashish.ghildiyal@appster.in>
2017-04-05 16:26:22 +0530

    changes color privacy & terms page

--- resources/views/terms-and-condition.blade.php
+++ resources/views/terms-and-condition.blade.php
@@ -1,33 +1,33 @@
 
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="description" content="">
-        <meta name="keywords" content="">
-        <meta name="author" content="">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-        <link href="//fonts.googleapis.com/css?family=Lato:300,400,700" rel="stylesheet">
-        <link rel="shortcut icon" href="img/favicon.ico" sizes="16x16" type="image/x-icon">
-        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <link href="//fonts.googleapis.com/css?family=Lato:300,400,700" rel="stylesheet">
+    <link rel="shortcut icon" href="img/favicon.ico" sizes="16x16" type="image/x-icon">
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-   
-        <title>DentaMatch</title>
-        <style>
-            body{background-color:rgb(240,240,240);padding:0 10px;}
-            p{color:rgb(62,57,53);}
-        </style>
+    
+    <title>DentaMatch</title>
+    <style>
+        body{background-color:rgb(240,240,240);padding:0 10px;}
+        p{color:#686868;}
+    </style>
 
-    </head>
+</head>
 
-    <body>
-        <div class="container doc-cont terms">
-            <h3 class="pg-heading mr-t-30 mr-b-30">Term and Condition</h3>
-            <p style="">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+<body>
+    <div class="container doc-cont terms">
+        <h3 class="pg-heading mr-t-30 mr-b-30">Term and Condition</h3>
+        <p style="">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
 
 
-        </div>
-    </body>
+    </div>
+</body>
 </html>
 
