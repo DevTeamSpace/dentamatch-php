46629c3d56072a9a350b608e20d4d843b77e0669
Ashish Ghildiyal <ashish.ghildiyal@appster.in>
2017-04-05 16:31:40 +0530

    hide title

--- resources/views/privacy-policy.blade.php
+++ resources/views/privacy-policy.blade.php
@@ -22,7 +22,7 @@
 
 <body>
     <div class="container doc-cont terms">
-        <h3 class="pg-heading mr-b-30">Privacy Policy</h3>
+     <!--  <h3 class="pg-heading mr-b-30">Privacy Policy</h3> -->
         <p style="">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
     </div>
 </body>
