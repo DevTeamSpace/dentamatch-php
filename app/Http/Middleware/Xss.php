c14a0d12a82bdf464ce3878873f029b7476c05f3
jasvinderSingh <jasvinder.singh@appster.in>
2017-04-10 15:23:02 +0530

    xss issue

--- app/Http/Middleware/Xss.php
+++ app/Http/Middleware/Xss.php
@@ -23,8 +23,8 @@ class Xss
         $input = $request->all();
 
         array_walk_recursive($input, function(&$input) {
-            if(!array($input))
-                $input = htmlspecialchars($input);
+            
+                $input = strip_tags($input);
 });
 
         $request->merge($input);
