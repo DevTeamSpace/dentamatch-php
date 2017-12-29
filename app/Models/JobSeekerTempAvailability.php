d2073382ce0bf91761b707a996e2f143bdb50ada
ravi_madan <ravi.madan@appster.in>
2017-12-29 12:57:41 +0530

    logging temp dates during signup

--- app/Models/JobSeekerTempAvailability.php
+++ app/Models/JobSeekerTempAvailability.php
@@ -26,7 +26,8 @@ class JobSeekerTempAvailability extends Model
         while($currentDate<=$endDate) {
                 $dateString = (int) date("w", $currentDate);
                 $insertDate = date( "Y-m-d",$currentDate);
-                if($dateString != 6 || $dateString!=0) {
+                sleep(1);
+                if($dateString!=6 || $dateString!=0) {
                     Log::info($dateString);
                     Log::info($insertDate);
                     $tempDateArray[] = array('user_id' => $userId, 'temp_job_date' => $insertDate);
