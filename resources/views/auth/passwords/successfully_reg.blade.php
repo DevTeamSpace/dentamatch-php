8236cddac9a17d35c39dbc26ac9ddf4c28ad6f76
shashank <Shashank@Ankush-Sharma155.local>
2018-01-30 13:01:51 +0530

    changes in api

--- resources/views/auth/passwords/successfully_reg.blade.php
+++ resources/views/auth/passwords/successfully_reg.blade.php
@@ -11,7 +11,7 @@
             </div>
             <div class="col-sm-6 nopadding col">
                 <div class="frm-inr-credbox bg-white ">
-                    Your passwrod is updated successfully.
+                    Your password is updated successfully.
                 </div>
             </div>
         </div>
