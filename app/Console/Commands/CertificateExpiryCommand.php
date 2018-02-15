d4dc8f05cda014f239fb1d0e057285c08968d641
shashank <Shashank@Ankush-Sharma155.local>
2018-02-15 09:21:45 +0530

    changes in api

--- app/Console/Commands/CertificateExpiryCommand.php
+++ app/Console/Commands/CertificateExpiryCommand.php
@@ -69,7 +69,7 @@ class CertificateExpiryCommand extends Command
                 $deviceModel = Device::getDeviceToken($userId);
                 if($deviceModel) {
                     NotificationServiceProvider::sendPushNotification($deviceModel, $notificationData['notificationData'], $params);
-                    $data = ['sender_id'=>$adminModel->id,'receiver_id'=>$userId, 'notification_data'=>$notificationData['message'],'notification_type'=>Notification::OTHER];
+                    $data = ['sender_id'=>$adminModel->id,'receiver_id'=>$userId, 'notification_data'=>$notificationData['notificationData'],'notification_type'=>Notification::OTHER];
                     Notification::createNotification($data);
                 }
             }
