cd98868743997e6d249665414c3cb545c1726dc6
jasvinderSingh <jasvinder.singh@appster.in>
2018-11-26 12:10:42 +0530

    add logout message

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Support\Facades\Auth;
+use Session;
 
 class Authenticate
 {
@@ -26,7 +27,10 @@ class Authenticate
         }
         $user = Auth::user();
         
-        if(isset($user) && $user->userGroup->group_id==1){
+        if(isset($user) && $user->is_active!=1){
+            Session::flash('message', trans("messages.deactivated_admin"));
+            return redirect("logout");
+        }elseif(isset($user) && $user->userGroup->group_id==1){
             return redirect("home");
         }else{
             return $next($request);
