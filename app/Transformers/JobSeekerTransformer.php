c6be2f3c322e98c13fd4d33dd2af71cffd2b9220
Catherine <catthr.grr@gmail.com>
2019-11-05 15:07:48 +0500

    csv fix

--- app/Transformers/JobSeekerTransformer.php
+++ app/Transformers/JobSeekerTransformer.php
@@ -15,16 +15,15 @@ class JobSeekerTransformer
 
     public function transformItem(JobSeekerProfiles $item) {
         $result = [];
-
         $result['first_name'] = $item->first_name;
         $result['last_name'] = $item->last_name;
         $result['email'] = $item->user->email;
         $result['phone'] = '';
         $result['license_number'] = $item->license_number;
         $result['license_state'] = $item->state;
-        $result['job_title'] = object_get($item, 'job_title.jobtitle_name');
+        $result['job_title'] = object_get($item, 'jobTitle.jobtitle_name');
         $result['skills'] = $item->user->skills->implode('skill_name', ',');
-        $result['preferred_location'] = object_get($item, 'preferred_location.preferred_location_name');
+        $result['preferred_location'] = object_get($item, 'preferredLocation.preferred_location_name');
         $result['school_name'] = $item->user->schooling->pluck('pivot.other_schooling')->filter()->implode(',');
         $result['registration_date'] = $item->user->created_at->toDateString();
         $result['verification_status'] = SeekerVerifiedStatus::toString($item->is_job_seeker_verified);
