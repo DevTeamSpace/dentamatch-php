b640a1d1730c25cbe1caf906b9ecfa71a7e1175c
Catherine <catthr.grr@gmail.com>
2019-11-12 17:32:33 +0500

    dm-30 mobile

--- resources/views/web/layouts/signup.blade.php
+++ resources/views/web/layouts/signup.blade.php
@@ -15,7 +15,7 @@
   <link rel="stylesheet" href="{{asset('web/css/bootstrap-custom-theme.css')}}">
 
   <link rel="stylesheet" href="{{asset('web/plugins/parsley/css/parsley.css')}}">
-  <link rel="stylesheet" href="{{asset('web/css/custom.css?v=1.3')}}">
+  <link rel="stylesheet" href="{{asset('web/css/custom.css?v=1.4')}}">
 
   @yield('css')
 
